4b2dbd98847d30a8c2f203e38eb09c8f038b4206
Stecuuuu <Stecalan123@wp.pl>
2020-12-25 16:55:30 +0100

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'ACTIVITY',
+        token: 'NzgyMzgyOTkzOTU1NjE4ODY2.X8LY8Q.VgPBgcv8ri4e4wO0gK_k8vQSsEA',
+        prefix: '^^',
+        activity: 'Wściekły najman je mandarynke!',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
